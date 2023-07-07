b2c9e3e03ea7a8ae892418a65197ee622fac2d5b
PujaPandit <133088772+PujaPandit@users.noreply.github.com>
2023-07-07 11:40:02 +0530

    Create SecondServlet.java

---
+++ SecondServlet.java
@@ -0,0 +1,40 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class SecondServlet
+ */
+@WebServlet("/servlet2")
+public class SecondServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public SecondServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		
+		String email=request.getParameter("em");
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		
+		out.println(email+"<h1>Welcome To Home Page</h1>");
+	}
+
+}
