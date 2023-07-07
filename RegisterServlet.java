9feb10b9ede8489a25e99f457863f76b6e4249c4
PujaPandit <133088772+PujaPandit@users.noreply.github.com>
2023-07-07 11:39:10 +0530

    Create RegisterServlet.java

---
+++ RegisterServlet.java
@@ -0,0 +1,53 @@
+package com.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class RegisterServlet
+ */
+@WebServlet("/register")
+public class RegisterServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegisterServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+//	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		// TODO Auto-generated method stub
+//		response.getWriter().append("Served at: ").append(request.getContextPath());
+//	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String name=request.getParameter("user-name");
+		String email=request.getParameter("email");
+		String password=request.getParameter("password");
+		String course=request.getParameter("course");
+		
+		response.setContentType("text/html");
+		PrintWriter out=response.getWriter();
+		out.print("<h1>Name: "+ name +"<h1>");
+		out.print("<h1>Email: "+ email +"<h1>");
+		out.print("<h1>Password: "+ password +"<h1>");
+		out.print("<h1>Course: "+ course +"<h1>");
+	}
+
+}
